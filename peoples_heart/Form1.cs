2c083a4eedb40618291a733b88410e9c84475f2f
Shahidul Islam <shawon_math07@yahoo.com>
2016-12-22 14:42:33 +0600

    metro form established

--- peoples_heart/Form1.cs
+++ peoples_heart/Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace peoples_heart
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MetroFramework.Forms.MetroForm
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
